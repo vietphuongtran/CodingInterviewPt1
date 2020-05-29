8d22afd1d274073fdd06bacea7a72d102f715241
Paul Tran <paultran0307@gmail.com>
2020-05-29 13:30:43 -0400

    complete project

--- TeamMember.js
+++ TeamMember.js
@@ -6,11 +6,14 @@
 //the getJSON will take in 2 arguments
 //the url where you get the JSON from
 //the callback method to see whether the url works
+//callback will take 2 arguments
+//the status and the response which will be a json object
 var getJSON = function(url, callback) {
     //send request to the URL
     var xhr = new XMLHttpRequest();
     //specify the  GET method
     xhr.open('GET', url, true);
+    //getback the Json file
     xhr.responseType = 'json';
     xhr.onload = function() {
         var status = xhr.status;
@@ -43,7 +46,7 @@ getJSON('http://sandbox.bittsdevelopment.com/code1/fetchemployees.php',
             // var crown = document.getElementById("crown");
             // console.log("name");
             // name.innerHTML = data[1].employeefname + ' ' + data[1].employeelname;
-            // //calculate is
+            // //cis Employee Featured
             // // function isEmployeeFeatured (point) {
             // //         if (point === "1") {
             // //             crown.innerHTML = "&#128081";
@@ -58,28 +61,41 @@ getJSON('http://sandbox.bittsdevelopment.com/code1/fetchemployees.php',
             // //should be roles[0].rolename because roles is an array
             // employeeroles.innerHTML = data[1].roles[0].rolename;
             // employeeroles.style.background = data[1].roles[0].rolecolor;
-            //Step 2: use the pattern to put it in a loop
+            //Step 2: use the pattern to put in a loop then get the rest
+
+            //     for (var k = 1; k <= Object.keys(data).length; k++) {
+            //         for (var j = 0; j <= 1; j++) {
+            //         console.log(data[k].roles[j].rolename)
+            //     }
+            // }
+            //Object.keys(data).length: how to get JSON length
             for (var i = 1; i <= Object.keys(data).length; i++) {
-                function isEmployeeFeatured (point) {
+                //testing .map method with rolename and rolecolor
+                // var rolename = data[i].roles.map(function (role) {
+                //     return role.rolename;
+                // });
+                // var rolecolor = data[i].roles.map(function (role){
+                //     return role.rolecolor;
+                // })
+                function isEmployeeFeatured(point) {
                     if (point === "1") {
                         return "&#128081";
                     } else {
                         return "";
                     }
                 }
-                var img = "http://sandbox.bittsdevelopment.com/code1/employeepics/" + data[i].employeeid +".jpg";
+                //get the image
+                var img = "http://sandbox.bittsdevelopment.com/code1/employeepics/" + data[i].employeeid + ".jpg";
                 employee.innerHTML +=
-                    "<div class= grid-item>" +
-                        //"<div>" + "<img src=http://sandbox.bittsdevelopment.com/code1/employeepics/1.jpg" + ">" + "</div>" +
-                        "<div class=picture><img class='employeeimg' src=" + img + " alt='Image of employee' /></div>" +
-                        "<div>"+ data[i].employeefname + ' ' + data[i].employeelname +
-                        "<span>" + isEmployeeFeatured (data[i].employeeisfeatured) + "</span>" + "</div>" +
-                        "<div>"+ data[i].employeebio + "</div>" +
-                        "<span class = roleColor>" + data[i].roles[0].rolename + "</span>" +
+                    "<div class= flexitem>" +
+                    "<div><img class='employeeimg' src=" + img + " alt='Image of employee' />" +
+                    "<span>" + isEmployeeFeatured(data[i].employeeisfeatured) + "</span></div>" +
+                    "<div class= employeename>" + data[i].employeefname + ' ' + data[i].employeelname + "</div>" +
+                    "<div>" + data[i].employeebio + "</div>" +
+                    //how to get the rolename and rolecolor
+                    //Answer: using map method to get the rolename as well as the rolecolor
+                    "<div>" + data[i].roles.map(function (role) {return "<span class = rolestyle style= background-color:" + role.rolecolor + ">" + role.rolename + "</span>"}).join ("    ") + "</div>" +
                     "</div>";
-                var roleColor = document.getElementsByClassName("roleColor");
-                var employeeimg = document.getElementsByClassName("employeeimg");
-                roleColor[i-1].style.background = data[i].roles[0].rolecolor;
             }
         }
     });
